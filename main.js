13e7c210ca66125f1920e5fd954acdb6a7fcf1cd
angelb <angelb1954@gmail.com>
2022-10-16 18:41:05 +0200

    cambio

--- main.js
+++ main.js
@@ -536,23 +536,29 @@ console.log(iterator.next().value);
 console.log(iterator.next());
 
 
-// for (const itItem of iterator) {
-//   console.log(itItem);
-// }
+for (const itItem of iterator) {
+  console.log(itItem);
+}
 
-// function* numero(){
-//   let contador= 0;
-//   while(true){
-//       yield contador;
-//       contador++;
-//   }
-// }
+function* numero(){
+  let contador= 0;
+  while(true){
+      yield contador;
+      contador++;
+  }
+}
 
-// const it2 = numero();
 
-// console.log(it2.next());
-// console.log(it2.next());
-// console.log(it2.next());
-// console.log(it2.next());
-// console.log(it2.next());
-// console.log(it2.next());
+const it2 = numero();
+
+console.log(it2.next());
+console.log(it2.next());
+console.log(it2.next());
+console.log(it2.next());
+console.log(it2.next());
+console.log(it2.next());
+git add .add
+
+// Closure
+
+
